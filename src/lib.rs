0dd600cbb791e3d64e49ae4de93ceb12900363f0
Adam Eury <euryadam@gmail.com>
2025-10-11 09:27:21 -0400

    Display details of argument, build, and test failures

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,30 @@
 #![doc = include_str!("../README.md")]
-use anyhow::{anyhow, Context};
+use anyhow::{anyhow, Context, Result};
 use colored::Colorize;
-use std::{fmt::Display, process::Command, str::FromStr};
+use std::{
+    collections::{BTreeMap, HashMap},
+    fmt::Display,
+    process::{Command, ExitStatus},
+    str::FromStr,
+};
+
+#[derive(Debug)]
+pub struct Output {
+    pub results: Vec<TestResult>,
+    pub error: Option<CargoError>,
+    pub status: ExitStatus,
+}
+
+impl Output {
+    #[must_use]
+    pub fn failed(&self) -> bool {
+        !self.status.success()
+            || self
+                .results
+                .iter()
+                .any(|result| result.status == Status::Fail)
+    }
+}
 
 #[must_use]
 /// Runs `cargo test` with any supplied extra arguments, and returns the
@@ -10,22 +33,85 @@ use std::{fmt::Display, process::Command, str::FromStr};
 /// # Panics
 ///
 /// If executing the `cargo test` command fails.
-pub fn get_cargo_test_output(extra_args: Vec<String>) -> String {
+pub fn get_cargo_test_output(extra_args: Vec<String>) -> Output {
     let mut cargo = Command::new("cargo");
     cargo.arg("test");
     cargo.args(extra_args);
-    let raw_output = cargo
+    let cargo_output = cargo
+        .env("CARGO_TERM_COLOR", "always")
         .output()
         .context(format!("{cargo:?}"))
-        .expect("executing command should succeed")
-        .stdout;
-    String::from_utf8_lossy(&raw_output).to_string()
+        .expect("executing command should succeed");
+
+    let stdout = String::from_utf8_lossy(&cargo_output.stdout).to_string();
+    let results = parse_test_results(&stdout).expect("parsing test results should succeed");
+    let mut output = Output {
+        results,
+        error: None,
+        status: cargo_output.status,
+    };
+    if !output.status.success() {
+        let stderr = String::from_utf8_lossy(&cargo_output.stderr);
+        output.error = Some(parse_error(&stderr));
+    }
+    output
+}
+
+/// Parses the standard output of `cargo test` into a vec of `TestResult`.
+///
+/// # Errors
+///
+/// Returns an error if the test output cannot be parsed.
+pub fn parse_test_results(test_output: &str) -> Result<Vec<TestResult>> {
+    let mut results = BTreeMap::new();
+    let failures = parse_test_failures(test_output).context("parsing test failures")?;
+
+    for line in test_output.lines() {
+        if let Some(mut result) = parse_line(line) {
+            if result.status == Status::Fail {
+                result.failure = failures.get(&result.test).cloned();
+            }
+            results.insert(result.test.clone(), result);
+        }
+    }
+
+    Ok(results.into_values().collect())
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct FailureInfo {
+    pub message: String,
+    pub location: String,
+}
+
+impl std::fmt::Display for FailureInfo {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "At {}\n{}", self.location, self.message)
+    }
 }
 
 #[must_use]
-/// Parses the standard output of `cargo test` into a vec of `TestResult`.
-pub fn parse_test_results(test_output: &str) -> Vec<TestResult> {
-    test_output.lines().filter_map(parse_line).collect()
+pub fn parse_test_failures(output: &str) -> Option<HashMap<String, FailureInfo>> {
+    let mut failures = HashMap::new();
+    let lines: Vec<&str> = output.lines().collect();
+
+    for (i, line) in lines.iter().enumerate() {
+        if line.starts_with("thread") && line.contains("panicked at") {
+            let (test, location) = line
+                .strip_prefix("thread '")?
+                .split_once("' panicked at ")
+                .map(|(test, loc)| (test.into(), loc.into()))?;
+            let message = lines[i + 1..]
+                .iter()
+                .take_while(|l| !l.is_empty())
+                .copied()
+                .collect::<Vec<_>>()
+                .join("\n");
+            failures.insert(test, FailureInfo { message, location });
+        }
+    }
+
+    Some(failures)
 }
 
 /// Parses a line from the standard output of `cargo test`.
@@ -45,8 +131,10 @@ pub fn parse_line(line: impl AsRef<str>) -> Option<TestResult> {
     };
     Some(TestResult {
         module,
+        test: test.into(),
         name: prettify(name),
         status: status.parse().ok()?,
+        failure: None,
     })
 }
 
@@ -94,8 +182,10 @@ fn prettify_module(module: &str) -> Option<String> {
 /// The (prettified) name and pass/fail status of a given test.
 pub struct TestResult {
     pub module: Option<String>,
+    pub test: String,
     pub name: String,
     pub status: Status,
+    pub failure: Option<FailureInfo>,
 }
 
 impl Display for TestResult {
@@ -103,10 +193,13 @@ impl Display for TestResult {
         match &self.module {
             Some(module) => write!(
                 f,
-                "{} {} – {}",
+                "{} {} – {}{}",
                 self.status,
                 module.bright_blue(),
-                self.name
+                self.name,
+                self.failure
+                    .as_ref()
+                    .map_or(String::new(), |failure| format!("\n{failure}"))
             ),
             None => write!(f, "{} {}", self.status, self.name),
         }
@@ -142,6 +235,58 @@ impl Display for Status {
             Status::Ignored => "?".bright_yellow(),
         };
         write!(f, "{status}")
+    }
+}
+
+#[derive(Debug, PartialEq)]
+pub struct CargoError {
+    message: String,
+    tip: Option<String>,
+}
+
+impl std::fmt::Display for CargoError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        writeln!(f, "{}\n", self.message)?;
+        if let Some(tip) = &self.tip {
+            writeln!(f, "    {tip}")?;
+        }
+        Ok(())
+    }
+}
+
+#[must_use]
+pub fn parse_error(stderr: &str) -> CargoError {
+    let lines: Vec<_> = stderr.lines().collect();
+
+    for (i, line) in lines.iter().enumerate() {
+        if line.contains("error") {
+            let message = line.trim().to_string();
+            let mut error = CargoError { message, tip: None };
+
+            let mut other_lines = Vec::new();
+            for next_line in &lines[i + 1..lines.len()] {
+                if next_line.contains("error:") || next_line.contains("warning:") {
+                    break;
+                }
+                if next_line.contains("Usage:") {
+                    break;
+                }
+                other_lines.push(next_line);
+            }
+
+            for line in &other_lines {
+                if line.trim().contains("tip:") {
+                    error.tip = Some(line.trim().to_string());
+                }
+            }
+
+            return error;
+        }
+    }
+
+    CargoError {
+        message: stderr.trim().to_string(),
+        tip: None,
     }
 }
 
@@ -200,49 +345,61 @@ mod tests {
             Case {
                 line: "test foo ... ok",
                 want: Some(TestResult {
+                    test: "foo".into(),
                     module: None,
                     name: "foo".into(),
                     status: Status::Pass,
+                    failure: None,
                 }),
             },
             Case {
                 line: "test foo::tests::does_foo_stuff ... ok",
                 want: Some(TestResult {
+                    test: "foo::tests::does_foo_stuff".into(),
                     module: Some("foo".into()),
                     name: "does foo stuff".into(),
                     status: Status::Pass,
+                    failure: None,
                 }),
             },
             Case {
                 line: "test tests::urls_correctly_extracts_valid_urls ... FAILED",
                 want: Some(TestResult {
+                    test: "tests::urls_correctly_extracts_valid_urls".into(),
                     module: None,
                     name: "urls correctly extracts valid urls".into(),
                     status: Status::Fail,
+                    failure: None,
                 }),
             },
             Case {
                 line: "test files::test::files_can_be_sorted_in_descending_order ... ignored",
                 want: Some(TestResult {
+                    test: "files::test::files_can_be_sorted_in_descending_order".into(),
                     module: Some("files".into()),
                     name: "files can be sorted in descending order".into(),
                     status: Status::Ignored,
+                    failure: None,
                 }),
             },
             Case {
                 line: "test files::test::foo::tests::files_can_be_sorted_in_descending_order ... ignored",
                 want: Some(TestResult {
+                    test: "files::test::foo::tests::files_can_be_sorted_in_descending_order".into(),
                     module: Some("files::test::foo".into()),
                     name: "files can be sorted in descending order".into(),
                     status: Status::Ignored,
+                    failure: None,
                 }),
             },
             Case {
                 line: "test files::test_foo::files_can_be_sorted_in_descending_order ... ignored",
                 want: Some(TestResult {
+                    test: "files::test_foo::files_can_be_sorted_in_descending_order".into(),
                     module: Some("files::test_foo".into()),
                     name: "files can be sorted in descending order".into(),
                     status: Status::Ignored,
+                    failure: None,
                 }),
             },
             Case {
@@ -252,14 +409,60 @@ mod tests {
             Case {
                 line: "test output_format::_concise_expects ... ok",
                 want: Some(TestResult {
+                    test: "output_format::_concise_expects".into(),
                     module: Some("output_format".into()),
                     name: "concise expects".into(),
                     status: Status::Pass,
+                    failure: None,
                 }),
             },
         ]);
         for case in cases {
             assert_eq!(case.want, parse_line(case.line));
         }
+    }
+
+    #[test]
+    fn parse_error_fn_returns_unexpected_argument_error() {
+        let error = r"error: unexpected argument '--ignore-included' found
+
+    tip: to pass '--ignore-included' as a value, use '-- --ignore-included'
+
+Usage: cargo test [OPTIONS] [TESTNAME] [-- [ARGS]...]
+
+For more information, try '--help'.";
+
+        let want = CargoError {
+            message: "error: unexpected argument '--ignore-included' found".to_string(),
+            tip: Some(
+                "tip: to pass '--ignore-included' as a value, use '-- --ignore-included'".into(),
+            ),
+        };
+        let got = parse_error(error);
+
+        assert_eq!(want, got);
+    }
+
+    #[test]
+    fn parse_test_results_fn_returns_test_result_with_failure_details() {
+        let output = include_str!("../testdata/failure_output.txt");
+
+        let want = vec![TestResult {
+            test: "machine::tests::machine_step_executes_one_instruction".into(),
+            module: Some("machine".into()),
+            name: "machine step executes one instruction".into(),
+            status: Status::Fail,
+            failure: Some(FailureInfo {
+                message: r"assertion `left == right` failed: Program counter should be incremented after executing a `halt` instruction
+    left: 2
+    right: 1
+note: run with `RUST_BACKTRACE=1` environment variable to display a backtrace".into(),
+                location: "crates/rmachine-core/src/machine.rs:133:9:".into(),
+            }),
+        }];
+
+        let got = parse_test_results(output).unwrap();
+
+        assert_eq!(want, got);
     }
 }
--- tests/cli.rs
+++ tests/cli.rs
@@ -1,5 +1,6 @@
 use assert_cmd::Command;
 use predicates::prelude::*;
+use regex::Regex;
 
 #[test]
 fn passing_include_ignored_flag_runs_ignored_tests() {
@@ -12,4 +13,28 @@ fn passing_include_ignored_flag_runs_ignored_tests() {
         .assert()
         .success()
         .stdout(predicate::eq("✔ ignored test\n"));
+}
+
+#[test]
+fn passing_include_ignored_flag_returns_unexpected_argument_error() {
+    Command::cargo_bin("cargo-testdox")
+        .unwrap()
+        .current_dir("testdata/testproj")
+        .arg("testdox")
+        .arg("--include-ignored")
+        .assert()
+        .failure()
+        .stderr(predicate::function(|output: &str| {
+            let stderr = strip_ansi_codes(output);
+            stderr.contains("error: unexpected argument '--include-ignored' found")
+                && stderr.contains(
+                    "tip: to pass '--include-ignored' as a value, use '-- --include-ignored'",
+                )
+                && !stderr.contains("Usage: cargo test")
+        }));
+}
+
+fn strip_ansi_codes(s: &str) -> String {
+    let re = Regex::new(r"\x1b\[[0-9;]*m").unwrap();
+    re.replace_all(s, "").to_string()
 }
